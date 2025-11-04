e399635ee72b35ca3cd229faa792c91c537278c5
Gina Ruiz <ginarruizcampos@gmail.com>
2025-11-04 16:11:42 -0600

    3.1.11

---
+++ 3.1.11   LAB   Exceptions simple checks, part 2/part 2.cpp
@@ -0,0 +1,93 @@
+#include <iostream>
+
+using namespace std;
+
+
+class LimitException : public exception {
+public:
+    const char* what() const throw() {
+        return "Value could exceed limit.";
+    }
+};
+
+class BoundedValue {
+private:
+    int value;
+    int minLimit;
+    int maxLimit;
+
+public:
+
+    BoundedValue(int val, int minL, int maxL) {
+        value = val;
+        minLimit = minL;
+        maxLimit = maxL;
+    }
+
+
+    void add(int amount) {
+        if (value + amount > maxLimit || value + amount < minLimit) {
+            throw LimitException();
+        }
+        value += amount;
+    }
+
+
+    void subtract(int amount) {
+        if (value - amount < minLimit || value - amount > maxLimit) {
+            throw LimitException();
+        }
+        value -= amount;
+    }
+
+    int getValue() {
+        return value;
+    }
+};
+
+int main(void) {
+    int val1, min1, max1;
+    int val2, min2, max2;
+    int op1, op2, op3;
+
+
+    cin >> val1 >> min1 >> max1;
+    cin >> val2 >> min2 >> max2;
+
+
+    cin >> op1 >> op2 >> op3;
+
+
+    BoundedValue obj1(val1, min1, max1);
+    BoundedValue obj2(val2, min2, max2);
+
+
+    try {
+        obj1.add(op1);
+    }
+    catch (LimitException& e) {
+        cout << e.what() << endl;
+    }
+
+
+    try {
+        obj1.subtract(op2);
+    }
+    catch (LimitException& e) {
+        cout << e.what() << endl;
+    }
+
+
+    try {
+        obj2.add(op3);
+    }
+    catch (LimitException& e) {
+        cout << e.what() << endl;
+    }
+
+
+    cout << obj1.getValue() << endl;
+    cout << obj2.getValue() << endl;
+
+    return 0;
+}
